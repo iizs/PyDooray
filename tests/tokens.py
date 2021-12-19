d5ced0e1b62298b120d1f2f6c8302a02b8a6d86b
Choi Kirin <choi.kirin@gmail.com>
2021-12-19 12:09:28 -0800

    Add instructions for tokens.py file

--- tests/tokens.py
+++ tests/tokens.py
@@ -1,3 +1,13 @@
+"""
+Execute following command to ignore local changes on this file
+
+$ git update-index --skip-worktree tests/tokens.py
+
+Execute following command to track local changes on this file again
+
+$ git update-index --no-skip-worktree tests/tokens.py
+
+"""
 API_TOKEN = '<Your API token>'
 MESSENGER_HOOK_URL = '<Your hook url>'
 MESSENGER_HOOK_ICON_URL = "<Your hook icon url>"
